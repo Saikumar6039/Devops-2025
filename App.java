b6777ac8a98d019db50ab221e9f4b2f536a773d0
Saikumar6039 <vsaikumar02@gmail.com>
2025-03-28 14:27:20 +0530

    Create App.java

---
+++ App.java
@@ -0,0 +1,14 @@
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello, Jenkins! Welcome to the Java CI/CD Pipeline.");
+        System.out.println("Running basic operations...");
+
+        int a = 10;
+        int b = 20;
+        System.out.println("Sum: " + (a + b));
+        System.out.println("Multiplication: " + (a * b));
+        System.out.println("Division: " + (b / a));
+
+        System.out.println("Application executed successfully!");
+    }
+}
